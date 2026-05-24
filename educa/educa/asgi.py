c628b0797d247d96c07ac818bd1451e292e3a0de
mahmoud20212 <occee123@gmail.com>
2026-05-24 18:44:15 +0300

    refactor asgi.py to set production settings and improve application initialization

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -8,22 +8,28 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+# 1. تغيير الإعداد الافتراضي ليكون الخاص بالإنتاج (prod) بدلاً من (local)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings.prod')
+
+# 2. تهيئة وتجهيز تطبيقات Django بالكامل لمنع خطأ AppRegistryNotReady
+django_asgi_app = get_asgi_application()
+
+# 3. استيراد مكتبات Channels والـ Routing الخاص بك بعد تهيئة جينقو تماماً
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings.local')
-
-django_asgi_app = get_asgi_application()
-
+# 4. بناء الـ Application بالشكل السليم
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
-            URLRouter(chat.routing.websocket_urlpatterns)
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
         )
     ),
 })
